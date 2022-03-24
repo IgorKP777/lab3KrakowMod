58f53e6448aa065e28632e9ea7dfbec168277716
Admin <igorkydryashov7k@gmail.com>
2022-03-24 18:31:03 +0800

    upd

--- graph_integral.py
+++ graph_integral.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 import integral
@@ -9,21 +8,21 @@ b = 4
 
 
 def graph_1():
-    y = lambda x: (x ** 2) * (math.e ** (-1.5 * x))
+    y = lambda x: (x ** 2) * (np.e ** (-1.5 * x))
     x = np.linspace(a, b, n)
     plt.plot(x, y(x))
     plt.show()
 
 
 def graph_2():
-    y = lambda x: math.sin(x) * (math.e ** (-1.5 * x))
+    y = lambda x: np.sin(x) * (np.e ** (-1.5 * x))
     x = np.linspace(a, b, n)
     plt.plot(x, y(x))
     plt.show()
 
 
 def graph_3():
-    y = lambda x: math.cos(x) * (math.e ** (-1.5 * x))
+    y = lambda x: np.cos(x) * (np.e ** (-1.5 * x))
     x = np.linspace(a, b, n)
     plt.plot(x, y(x))
     plt.show()
