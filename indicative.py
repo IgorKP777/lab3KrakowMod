3253e2ab20c53a27238c1e19fd09476a715261c8
Admin <igorkydryashov7k@gmail.com>
2022-03-31 13:34:36 +0800

    upd

--- indicative.py
+++ indicative.py
@@ -9,10 +9,9 @@ n = 10000
 def indicative_1() -> tuple[str, float]:
     lt = list()
     for i in range(n):
-        x = -numpy.log(random.uniform(0, 1)) / 1.5
+        x = -numpy.log(random.random()) / 1.5
         if 1 <= x <= 4:
             lt.append((x ** 2) / 1.5)
-
     return 'Монте-Карло (показательный закон)', round(sum(lt) / n, 4)
 
 
@@ -22,7 +21,6 @@ def indicative_2() -> tuple[str, float]:
         x = -numpy.log(random.uniform(0, 1)) / 1.5
         if 1 <= x <= 4:
             lt.append((x ** 2) / 1.5)
-
     return 'Монте-Карло (показательный закон)', round(sum(lt) / n, 4)
 
 
@@ -32,5 +30,4 @@ def indicative_3() -> tuple[str, float]:
         x = -numpy.log(random.uniform(0, 1)) / 1.5
         if 1 <= x <= 4:
             lt.append((x ** 2) / 1.5)
-
     return 'Монте-Карло (показательный закон)', round(sum(lt) / n, 4)
